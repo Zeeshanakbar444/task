5f852ad8bc140d7d4ec9d1790cd5c98141c2ddc2
Akbarzeeshan <akbarzeeshan444@gmail.com>
2023-05-08 17:57:25 +0500

    for task

---
+++ app.js
@@ -0,0 +1,15 @@
+function data(){
+    var user = document.querySelector('#userName').value
+    var Fname = document.querySelector('#FatherName').value
+    var Roll = document.querySelector('#Roll').value
+    var select = document.querySelector('#select').value
+    var rad = document.querySelector('#rad').value
+
+console.log(user)
+console.log(Fname)
+console.log(Roll)
+console.log(rad)
+console.log(select)
+var h1 = document.querySelector('#h1')
+h1.innerHTML = user
+}
