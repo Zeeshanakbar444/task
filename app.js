535569a5c4642540c9d96d8ac7287cc623eeeb80
Akbarzeeshan <akbarzeeshan444@gmail.com>
2023-05-10 02:06:25 +0500

    task

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
-function data(){
+window.data=function () {
     var user = document.querySelector('#userName').value
     var Fname = document.querySelector('#FatherName').value
-    var Roll = document.querySelector('#Roll').value
+    var day = document.querySelector('#rad').value
     var select = document.querySelector('#select').value
-    var rad = document.querySelector('#rad').value
+    var rollNumber = document.querySelector('#roll_no').value
+    
 
-console.log(user)
-console.log(Fname)
-console.log(Roll)
-console.log(rad)
-console.log(select)
-var h1 = document.querySelector('#h1')
-h1.innerHTML = user
+    var one = document.querySelector('#one')
+    var two = document.querySelector('#two')
+    var three = document.querySelector('#three')
+    var four = document.querySelector('#four')
+    var five = document.querySelector('#five')
+
+   
+    
+
+    // one.innerHTML = `Name : ${user}`
+    // two.innerHTML = `Father Name : ${Fname}`
+    // three.innerHTML = `Roll No : ${rollNumber}`
+    // four.innerHTML = `Day : ${day}`
+    // five.innerHTML = `Subject : ${select}`
+
+    var studentData = {
+        studentName: user,
+        fatherName: Fname,
+        RollNO: rollNumber,
+        Day: day,
+        corseName: select
+    
+    }
+
+    localStorage.setItem('StudentName' , JSON.stringify(studentData));
 }
+
+
+   
+    
+        
+    var getData = localStorage.getItem('StudentName');
+    getData = JSON.parse(getData)
+    console.log(getData)
+
+     one.innerHTML = `Name : ${getData.studentName}`
+    two.innerHTML = `Father Name : ${getData.fatherName}`
+    three.innerHTML = `Roll No : ${getData.RollNO}`
+    four.innerHTML = `Day : ${getData.Day}`
+    five.innerHTML = `Subject : ${getData.corseName}`
